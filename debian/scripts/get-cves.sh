988ae720adb431c424180fbed34521a0de3cdd26
Andres Salomon <dilinger@queued.net>
2022-10-11 20:51:16 -0400

    Add get-cves.sh script to grab CVE entries from blog post
    I use this to pull down CVEs to cut & paste into debian/changelog.
    
    The following will output a list of the 20 or so CVEs from this
    blog entry:
    
    ./get-cves.sh https://chromereleases.googleblog.com/2022/08/stable-channel-update-for-desktop_30.html

---
+++ debian/scripts/get-cves.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+test "$#" -eq 1 || {
+  echo "Usage: $0 [URL]" 1>&2
+  exit 1
+}
+
+which elinks >/dev/null || {
+  echo 'Error: elinks missing. Do a `sudo apt-get install elinks`.' 1>&2
+  exit 1
+}
+
+echo
+elinks -dump "$1" 2>/dev/null | \
+  tr -d '\n' | \
+  sed -e 's/[[:space:]]\+/ /g' | \
+  sed -ne 's/CVE-/\n    - CVE-/pg' | \
+  sed -e 's/ on [0-9][0-9].*$/./' | \
+  grep -v 'CVE-[0-9\-]\+ exists in the wild' | \
+  tail -n +2
+echo
+
+exit 0

d1cbba22236bba111b22748858ba1dc306f6667d
Daniel Richard G. <skunk@iSKUNK.ORG>
2026-08-29 21:32:10 -0400

    Tell pre-gen to avoid the problematic glic targets

--- debian/deb_pre_gen.py
+++ debian/deb_pre_gen.py
@@ -193,12 +193,17 @@ handled_python_indep_scripts = set([
   "../../ui/webui/resources/tools/minify_js.py",  # fails with Node.js v12
 ])
 
-# Don't make these, as they depend on files we've excluded
 bad_outputs = set([
+  # Don't make these, as they depend on files we've excluded
   "gen/third_party/devtools-frontend/src/front_end/models/trace/lantern/core/unittests-tsconfig.json",
   "gen/third_party/devtools-frontend/src/front_end/models/trace/lantern/metrics/unittests-tsconfig.json",
   "gen/third_party/devtools-frontend/src/front_end/models/trace/lantern/simulation/unittests-tsconfig.json",
   "gen/third_party/devtools-frontend/src/frontend_indexer_tsconfig-tsconfig.json",
+
+  # Avoid running gen_enum_conversions.ts
+  "gen/chrome/browser/resources/glic/glic_api_impl/glic_api_injected_client.rollup.js",
+  "gen/chrome/browser/resources/glic/glic_api_injection_bundled/glic_api_impl/glic_api_injected_client.rollup.js",
+  "gen/chrome/browser/resources/glic/tsconfig_build_ts.json",
 ])
 
 # Parses toolchain.ninja files to determine the set of output files that
